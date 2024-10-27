e6e83b065b5b9ef7ba517345a58a16be41455fa6
jorik van looy <jorik716@gmail.com>
2024-10-27 19:20:12 +0100

    test the pipelines

--- spoke/terraform.tf
+++ spoke/terraform.tf
@@ -20,3 +20,15 @@ provider "azurerm" {
 data "azurerm_resource_group" "rg" {
   name = "rg-${var.env}"
 }
+
+resource "azurerm_storage_account" "example" {
+  name                     = "storageaccountname"
+  resource_group_name      = data.azurerm_resource_group.rg.name
+  location                 = data.azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "${var.env}"
+  }
+}
