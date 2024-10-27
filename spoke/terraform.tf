13e9585bbf56687aff66c238ce3c3e7944298ebd
jorik van looy <jorik716@gmail.com>
2024-10-27 19:32:42 +0100

    remove storage account made

--- spoke/terraform.tf
+++ spoke/terraform.tf
@@ -21,14 +21,3 @@ data "azurerm_resource_group" "rg" {
   name = "rg-${var.env}"
 }
 
-resource "azurerm_storage_account" "example" {
-  name                     = "storageaccountname2"
-  resource_group_name      = data.azurerm_resource_group.rg.name
-  location                 = data.azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
-
-  tags = {
-    environment = "${var.env}"
-  }
-}
