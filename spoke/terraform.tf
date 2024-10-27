a7d990899996eaec0aa69b841aac3e9bd095064f
jorik van looy <jorik716@gmail.com>
2024-10-27 19:58:12 +0100

    initial setup aca with azure monitoring

--- spoke/terraform.tf
+++ spoke/terraform.tf
@@ -21,3 +21,35 @@ data "azurerm_resource_group" "rg" {
   name = "rg-${var.env}"
 }
 
+resource "azurerm_log_analytics_workspace" "workspace" {
+  name                = "workspace-${env}"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+resource "azurerm_container_app_environment" "aca_env" {
+  name                       = "${var.env}-environment"
+  location                   = data.azurerm_resource_group.rg.location
+  resource_group_name        = data.azurerm_resource_group.rg.name
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.workspace.id
+}
+
+resource "azurerm_container_app" "aca" {
+  name                         = "${var.env}-app"
+  container_app_environment_id = azurerm_container_app_environment.aca_env.id
+  resource_group_name          = data.azurerm_resource_group.rg.name
+  revision_mode                = "Single"
+
+  template {
+    container {
+      name   = "java react example app"
+      image  = "jorikvl/cloud-devops-example:latest"
+      cpu    = 0.25
+      memory = "0.5Gi"
+
+    }
+  }
+
+}
