ac289ed960797bdfc56854958308e0ca4024156f
jorik van looy <jorik716@gmail.com>
2024-10-28 17:56:49 +0100

    still some typo's

--- spoke/outputs.tf
+++ spoke/outputs.tf
@@ -1,7 +1,7 @@
 output "client_id_managed_identity" {
-  value = azurerm_user_assigned_identity.mi_deploy_aca.client_id
+  value = azurerm_user_assigned_identity.mi-deploy-aca.client_id
 }
 
 output "tenant_id_managed_identity" {
-  value = azurerm_user_assigned_identity.mi_deploy_aca.tenant_id
+  value = azurerm_user_assigned_identity.mi-deploy-aca.tenant_id
 }
